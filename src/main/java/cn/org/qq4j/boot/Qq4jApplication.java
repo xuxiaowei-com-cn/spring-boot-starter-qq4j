a0f0490abb6c089a718b2a486b7c3aaf5dae22f2
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-27 12:33:12 +0800

    Initial commit

---
+++ src/main/java/cn/org/qq4j/boot/Qq4jApplication.java
@@ -0,0 +1,19 @@
+package cn.org.qq4j.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序执行入口
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@SpringBootApplication
+public class Qq4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Qq4jApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/org/qq4j/boot/Qq4jApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.org.qq4j.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qq4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
